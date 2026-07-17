036f444a56ad64bcd2005a8f999349efc85f5ada
Himmat Magar <himmatmagar007@gmail.com>
2026-07-17 19:44:36 +0545

    Simple Code

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from rag!")
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+# llm = ChatOpenAI(temperature=0.4)
+# reponsens = llm.invoke("What is the capital city of Nepal?")
+# print(reponsens.content)
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital city of Nepal")
+print(result.content)
