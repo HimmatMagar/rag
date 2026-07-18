01a5ebbe0397bb41effcdf5d2d3fc08eedd04e76
Himmat Magar <himmatmagar007@gmail.com>
2026-07-18 11:08:11 +0545

    text loader

---
+++ document_loaders.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template="Write a short summary about {topic}",
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader("text.txt")
+
+docs = loader.load()
+
+chain = prompt | model | parser
+print(chain.invoke({'topic': docs[0].page_content}))
