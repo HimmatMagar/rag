91317ce3b560847fd4b4154c982d96de1cfbb563
Himmat Magar <himmatmagar007@gmail.com>
2026-07-19 13:56:18 +0545

    directory loader

--- document_loaders.py
+++ document_loaders.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader, DirectoryLoader
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+'''
+Text Loader
+'''
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.1-8B-Instruct",
     task="text-generation"
@@ -26,3 +30,25 @@ docs = loader.load()
 
 chain = prompt | model | parser
 print(chain.invoke({'topic': docs[0].page_content}))
+
+
+'''
+PDF Loader
+'''
+pdf_loader = PyPDFLoader(file_path="docs.pdf")
+
+docs = pdf_loader.load()
+print(docs[0].metadata)
+print(docs[0].page_content)
+
+
+'''
+Directory Loader
+'''
+
+loader = DirectoryLoader(
+    path='folder_name',
+    glob="*.pdf", # all the pdf
+    loader_cls=PyPDFLoader
+)
+loader.lazy_load()
