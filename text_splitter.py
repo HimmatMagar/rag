ef423fb826297a291a3e397abe3cf27b165e269d
Himmat Magar <himmatmagar007@gmail.com>
2026-07-27 16:31:49 +0545

    structure splitter

--- text_splitter.py
+++ text_splitter.py
@@ -1,4 +1,4 @@
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
 
@@ -13,11 +13,42 @@ Disadvantages: Semantic meaning, grammer, structure
 """
 
 
-split = CharacterTextSplitter(
-    chunk_size=40,
-    chunk_overlap=0,
-    separator="\n"
+# split = CharacterTextSplitter(
+#     chunk_size=40,
+#     chunk_overlap=0,
+#     separator="\n"
+# )
+
+# char = split.split_documents(docs)
+# print(char[0])
+
+
+
+"""
+Recursive Character Text Splitter
+
+It follows a structure: \n\n -> Paragraph
+\n -> line
+-  -> words
+"" -> character
+"""
+
+text = """""
+Machine learning (ML) is a field of study in artificial intelligence concerned
+with the development and study of statistical algorithms that can learn from data
+and generalize to unseen data, and thus perform tasks without being explicitly programmed.
+Advances in the field of deep learning have allowed neural networks, a class of statistical algorithms,
+to surpass many previous machine learning approaches in performance.
+
+Statistics and mathematical optimisation methods compose the foundations
+of machine learning. Data mining is a related field of study, focusing on exploratory
+data analysis (EDA) through unsupervised learning.[3][4]
+"""""
+
+split = RecursiveCharacterTextSplitter(
+    chunk_size=150,
+    chunk_overlap=0
 )
 
-char = split.split_documents(docs)
-print(char[0])
+chunk = split.split_text(text)
+print(chunk)
