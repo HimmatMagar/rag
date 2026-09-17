adbe462c016d4b53a059e830f4291ccd662b34f1
Himmat Magar <himmatmagar007@gmail.com>
2026-09-17 12:30:54 +0545

    understanding llm basic

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from langchain_deepseek import ChatDeepSeek
+import os
 from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
 
 load_dotenv()
 
-llm = ChatDeepSeek(
-    model="deepseek-chat"
+
+llm = HuggingFaceEndpoint(
+    repo_id='deepseek-ai/DeepSeek-R1',
+    huggingfacehub_api_token=os.getenv("HUGGINGFACE_ACCESS_TOKEN"),
+    task='text-generation',
+    temperature=0.5,
+    max_new_tokens=50
 )
 
 
-response = llm.invoke("Hello")
-print(response)
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("what is machine learning")
+print(response.content)
